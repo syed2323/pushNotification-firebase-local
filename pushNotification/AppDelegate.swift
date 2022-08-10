223d5a0b50e5aa450fbfe932e2624698abe23bad
syed2323 <syedahad921@gmail.com>
2022-08-10 18:51:46 +0500

    push notification first commit

--- pushNotification/AppDelegate.swift
+++ pushNotification/AppDelegate.swift
@@ -6,15 +6,38 @@
 //
 
 import UIKit
+import Firebase
+import UserNotifications
+import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge,. sound]) { success, _ in
+            guard success else{
+                return
+            };print("success in APNS registry")
+        }
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+
+            guard let token = token else {
+                return
+            }
+            print("token: \(token)")
+        }
     }
 
     // MARK: UISceneSession Lifecycle
--- pushNotification/ViewController.swift
+++ pushNotification/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    var noOfnotification = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        notificationCenter.delegate = self
+        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (success, error)  in
+            
+        }
     }
+    
+    @IBAction func btnLoacalNotificationClick (_ sender: UIButton) {
+        print("button click")
+        noOfnotification = noOfnotification + 1
+        let content = UNMutableNotificationContent()
+        content.categoryIdentifier = "My Catgory Identifier"
+        content.title = "local notification"
+        content.body = "This is created example"
+        content.badge = noOfnotification as NSNumber
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
+        let identifier = "Main identifier"
+        let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
+        notificationCenter.add(request) { (error) in
 
-
+            print("error : \(error?.localizedDescription)")
+        }
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 }
 
